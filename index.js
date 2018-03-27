93c00913d0960c94686450a5f5c877c6c8bbaf7f
Sarah Kenney <sarah.elizabethj22@gmail.com>
2018-03-27 16:52:53 -0600

    added code to html and js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function getRecipeJson() {
-            var apiKey = "your-api-key-here";
-            var TitleKeyword = "lasagna";
-            var url = "http://api2.bigoven.com/recipes?pg=1&rpp=25&Title_kw="
-                  + TitleKeyword 
-                  + "&api_key="+apiKey;
-            $.ajax({
-                    type: "GET",
-                    dataType: 'json',
-                    cache: false,
-                    url: url,
-                    success: function (data) {
-                    alert('success');
-                    console.log(data);
-                    }
-                });
-        }
+
+
+
+function renderHomeScreen () {
+    console.log('open home screen')
+}
+
+function renderSearchResults () {
+    console.log('open results page')
+}
+
+function renderApp () {
+    renderHomeScreen();
+    renderSearchResults();
+}
+
+$(renderApp)
