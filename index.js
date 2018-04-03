8fcfe987ed1d20977b38d8c1e879254c42b5bd08
Sarah Kenney <sarah.elizabethj22@gmail.com>
2018-04-03 10:39:22 -0600

    completed first version of product

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 const CENSUS_SEARCH_URL = 'https://api.census.gov/data/2016/acs/acsse/';
 const CENSUS_KEY = '67eacecacd439e8e5bdea9feea8ae0281d28c761';
 
-
-function getApiData (searchTerm,callback) {
-    let c = $(`.js-cityInput`).val()
-    let s = $(`.js-stateInput`).val()
-    const settings = {
-        url: CENSUS_SEARCH_URL,
-        data: {
-            get: 'NAME,K200802_002E,K200802_003E,K200802_004E,K200802_005E',
-            for: `place:${c}`,
-            in: `state:${s}`,
-            key: CENSUS_KEY,
-        },
-        datatype: 'json',
-        type: 'GET',
-        success: callback,
-    };
-$.ajax(settings)
-}
-
 function renderHomeScreen () {
     console.log('open home screen')
-    return `<form class="js-searchForm">
-                <input type="text" class="js-cityInput" placeholder="City"></input>
-                <input type="text" class="js-stateInput" placeholder="State"></input>
-                <button type="input">Search</button>
+    return `<form class="js-searchForm searchForm">
+                <input type="text" class="js-cityInput cityInput" placeholder="City"></input>
+                <input type="text" class="js-stateInput stateInput" placeholder="State"></input>
+                <button type="input" class="searchButton">Search</button>
             </form>` 
 }
 
@@ -33,33 +14,98 @@ function displayHomeScreen () {
     $(`.js-container`).html(renderHomeScreen());
 }
 
-function renderSearchResults (item) {
-    return `<div>
-                <p>Total people with commuting time less than 10 minutes: ${item[1]}</p>
-            </div>`;
+
+function getApiDataTotal (callback) {
+    const settings = {
+        url: CENSUS_SEARCH_URL,
+        data: {
+            get: 'NAME,K200802_002E,K200802_003E,K200802_004E,K200802_005E',
+            for: `place:*`,
+            key: CENSUS_KEY,
+        },
+        datatype: 'json',
+        type: 'GET',
+        success: callback,
+    };
+    $.ajax(settings)
+}
+
+
+function findIndexLocation (element) {
+    let cityQuery = $(`.js-cityInput`).val();
+    let stateQuery = $(`.js-stateInput`).val();
+    const city = cityQuery.toLowerCase();
+    const state= stateQuery.toLowerCase();
+    if (element[0].toLowerCase().indexOf(city)!=-1 && element[0].toLowerCase().indexOf(state)!=-1)  {
+        return true;
+    }
+}
+
+
+
+function returnSearchResults (dataTotal) {
+    console.log('Total Results:', dataTotal)
+ 
+    const index = dataTotal.findIndex(findIndexLocation)
+        if (index!==-1) {
+            return `<div class="results">
+                        <p>Less than 10 minutes: ${dataTotal[index][1]}</p>
+                        <p>Between 10 and 29 minutes: ${dataTotal[index][2]}</p>
+                        <p>Between 30 and 59 minutes: ${dataTotal[index][3]}</p>
+                        <p>Over 60 minutes: ${dataTotal[index][4]}</p>
+                        <button class="js-again again">Look Up Another Location</button>
+                    </div>`
+        }
+        else {
+            return `<div class= "error">
+                        <p>Please enter a valid city and state</p>
+                        <button class="js-again again">Try Again</button>
+                    </div>`
+        }
+}     
+
+
+function displaySearchResults (dataTotal) {
+    console.log('This should show results')
+    $(`.js-container`).html(returnSearchResults(dataTotal));
 
 }
 
-function displaySearchResults (data) {
-    console.log('results:', data);
-    let results = $.map((item,index) =>
-        renderSearchResults(item));
-    $(`.js-container`).html(renderSearchResults(results));
+function determineHappiness () {
+    console.log(happyData[0])
+    const happyindex= happyData.findIndex(findIndexLocation)
 
+        return `<div class="happyParagraph">
+                    <p>${happyData[happyindex][0]} is ranked number ${happyData[happyindex][1]} happiest cities in the US!</p>
+                </div>`;
 }
+
+function displayHappiness (happyData) {
+    $(`.js-happiness`).html(determineHappiness(happyData));
+}
+
 
 function watchSubmitResults () {
     console.log('show results')
     $(`.js-container`).on('submit', '.js-searchForm', function (event) {
         event.preventDefault();
-        let query = $(`.js-input`).val();
-        getApiData(query, displaySearchResults);
+        getApiDataTotal(displaySearchResults);
+        displayHappiness();
+        
     });
+}
+
+function findNewLocation() {
+    $(`.js-container`).on('click', '.js-again', function (event) {
+        displayHomeScreen();
+        $(`.happyParagraph`).remove();
+    })
 }
 
 function renderApp () {
     displayHomeScreen();
     watchSubmitResults();
+    findNewLocation();
 }
 
 $(renderApp)
